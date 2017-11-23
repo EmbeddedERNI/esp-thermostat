35de1e479323d0e57ff7ec907e6690f845071592
hmb <hernan.bartoletti@gmail.com>
2017-11-23 10:19:57 +0100

    Update dht22.h

--- main/dht22.h
+++ main/dht22.h
@@ -1,3 +1,10 @@
+/**
+ *  @brief     Proof of concept of a simple thermostat using a ESP32 module and a DHT22 sensor.
+ *
+ *  @file      dht22.c
+ *  @author    Hernan Bartoletti - hernan.bartoletti@gmail.com
+ *  @copyright MIT License
+ */
 #include <stdint.h>
 
 void dht22_init(void);
